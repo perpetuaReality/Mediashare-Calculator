803a8995b29b098d251b5a49d938d665787ab2ba
perpetuareality <35542725+perpetuaReality@users.noreply.github.com>
2026-02-20 22:55:32 -0300

    Initial commit.

---
+++ server.js
@@ -0,0 +1,62 @@
+import express from "express"
+import { parse } from "tinyduration"
+
+const app = express()
+const port = 3000
+
+process.loadEnvFile(".env")
+
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+	res.sendFile("index.html")
+})
+
+const VIDEO_NOT_FOUND_ERR = {
+	error: "Couldn't find that video! Please check the ID.",
+}
+const AUTHORIZATION_ERR = {
+	error: "An authorization error occurred! Please contact Perp.",
+}
+
+const SECONDS_IN_A_DAY = 60 * 60 * 24
+const SECONDS_IN_AN_HOUR = 60 * 60
+const SECONDS_IN_A_MINUTE = 60
+
+app.get("/videoLength/:id", (req, res) => {
+	const videoID = req.params.id
+	if (videoID.length !== 11) return res.status(400).send(VIDEO_NOT_FOUND_ERR)
+
+	fetch(
+		`https://www.googleapis.com/youtube/v3/videos?id=${videoID}&key=${process.env.YOUTUBE_KEY}&part=contentDetails&fields=items(contentDetails/duration)`,
+	)
+		.then((res) => res.json())
+		.then((body) => {
+			if (!body.error?.code) {
+				if (!body.items.length)
+					return res.status(400).send(VIDEO_NOT_FOUND_ERR)
+
+				const videoDuration = body.items[0].contentDetails.duration
+				const durationParts = parse(videoDuration)
+				const videoSeconds =
+					(durationParts.days ?? 0) * SECONDS_IN_A_DAY +
+					(durationParts.hours ?? 0) * SECONDS_IN_AN_HOUR +
+					(durationParts.minutes ?? 0) * SECONDS_IN_A_MINUTE +
+					(durationParts.seconds ?? 0)
+				return res.status(200).send({ duration: videoSeconds })
+			}
+
+			console.error(body)
+			switch (body.error.code) {
+				case 400:
+				case 404:
+					res.status(400).send(VIDEO_NOT_FOUND_ERR)
+				case 403:
+					res.status(400).send(AUTHORIZATION_ERR)
+			}
+		})
+})
+
+app.listen(port, () => {
+	console.log(`Mediashare calculator listening on port ${port}`)
+})
